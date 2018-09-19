7517cfe37e5fb05055bf362592f31e39660a784b
Matheus Compri <matheus.compri@gmail.com>
2018-09-18 22:42:43 -0300

    feat: improving project and adding tests to CommentBox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ComponentBox = () => {
-  return (
-    <div>
-      Comment Box
-    </div>
-  );
+class ComponentBox extends Component {
+
+  state = {
+    comment: ''
+  }
+
+  _handleChange = (evt)=>  {
+    this.setState({ comment: evt.target.value });
+  }
+
+  _handleSubmit = (evt)=>  {
+    evt.preventDefault();
+
+    // TODO - Call an action creator to save the comment
+
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this._handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea onChange={this._handleChange} value={this.state.comment} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
 };
 
 export default ComponentBox;
